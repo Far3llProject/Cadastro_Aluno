87da579870965255cccaa63896508fc08ed0179f
Rafael Martins de Castro <42079080+far3llproject@users.noreply.github.com>
2019-10-12 15:17:36 -0300

    completando os requisitos solicitados em sala de aula

--- app/src/main/java/com/example/cadastro_aluno/Cadastro.java
+++ app/src/main/java/com/example/cadastro_aluno/Cadastro.java
@@ -1,12 +1,16 @@
 package com.example.cadastro_aluno;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -18,17 +22,38 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-
+//--------------------------------------------------------
         alunoRA = findViewById(R.id.edtRA);
         Nome = findViewById(R.id.edtNome);
-        Cursos = findViewById(R.id.spinCurso);
+        //DECLARAÇÃO DO SPINNER
+        Spinner combo = (Spinner)findViewById(R.id.spinCurso);
+//--------------------------------------------------------
+        //RECUPERANDO O ARRAY DE STRINGS DO STRINGS.XML
+        String[] cursos = getResources().getStringArray(R.array.cursos);
+        //AGORA NOSSO SPINER PRECISA DE UM ADAPTER
+        ArrayAdapter<String> adp = new ArrayAdapter<>
+              ( this, android.R.layout.simple_spinner_item,cursos);
+        //INTEGRAR AS STRIGS AO MEU OBJETO DA ACTIVITY SPINNER
+        combo.setAdapter(adp);
 
-    }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.cadastro, menu);
+        inflater.inflate(R.menu.menu_cadastro, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.cadSalvar){
+            Toast.makeText(this,"Salvando...", Toast.LENGTH_LONG).show();
+            finish();
+            }
+        if (id == R.id.cadCancel){
+            finish(); }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/cadastro_aluno/MainActivity.java
+++ app/src/main/java/com/example/cadastro_aluno/MainActivity.java
@@ -2,54 +2,61 @@ package com.example.cadastro_aluno;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+//DECLARAÇÃO DOS OBJETOS NA ACTIVITY
 ListView ListaAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//---------------------------------------------------------------------------------
+        //ESTÂNCIANDO OS OBJETOS
         ListaAluno = findViewById(R.id.lstAluno);
-
+        //CRIANDO UMA LISTA COM ARRAY
         List<Object> objetos = new ArrayList<>();
-
+        //CRIO UM ADAPTER PARA INTEGAR MEU OBJETO DA ACTIVITY COM MEU ARRAY
         ArrayAdapter<Object> adp = new ArrayAdapter<>
                 (this, android.R.layout.simple_gallery_item, objetos);
-
+        //DAR UM SET NO MEU OBJETO DA ACTIVITY
         ListaAluno.setAdapter(adp);
-
+//---------------------------------------------------------------------------------
 
     }
+
+//MENU ACOPLADO A MINHA ACTIVITY
+//O MENU É TRATADO SEPARADO EM UMA OUTRA XML
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //PARA ACOPLAR NA MINHA ACTIVITY DEVO CRIAR UM INFLATER
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_ista_aluno, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+//DAR FUNÇÃO AOS ITENS DO MENU
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
+        //PARA CADA ITEM CRIAR UMA CONDIÇÃO
+        //PODE SER USADO TAMBÉM UM SWITCH CASE
         int id = item.getItemId();
 
         if (id == R.id.nvaluno) {
-            return true;
-       }
-
+         Intent intencao = new Intent(this, Cadastro.class);
+        startActivity(intencao);}
+        if (id == R.id.FechaApp){
+            finish();
+        }
         return super.onOptionsItemSelected(item);
     }
 }
